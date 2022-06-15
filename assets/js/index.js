9c7db05c001a865211b4b9eb40499c61fa00f50e
Katie Durga <katie.durga@gmail.com>
2022-06-14 23:26:48 -0600

    component

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Component extends Component {
+  render() {
+    return (
+      <div>
+        shmatie
+      </div>
+    )
+  }
+}
